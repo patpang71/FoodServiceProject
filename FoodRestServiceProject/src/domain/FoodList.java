dd7793fa08532697a173c1a9600e7072d5607999
Patrick <patpang71@gmail.com>
2017-12-02 15:13:19 -0800

    Food Service Project 2nd

---
+++ FoodRestServiceProject/src/domain/FoodList.java
@@ -0,0 +1,34 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSeeAlso;
+
+@XmlRootElement
+@XmlSeeAlso(Food.class)
+public class FoodList extends ArrayList<Food> {
+	private final static long serialVersionUID = 1L;
+	
+	public FoodList(Collection<? extends Food> c) {
+		super(c);
+	}
+	
+	public FoodList() {
+		super();
+	}
+	
+	@XmlElement(name="Foods")
+	public List<Food> getList() {
+		return this;
+	}
+	
+	public void addFoods(List<Food> foods) {
+		this.addAll(foods);
+	}
+	
+	
+}
